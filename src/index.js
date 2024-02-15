bfd4e4f804074b244cb9ed5914c571d2409cc7de
pawelek <k.pawelek@vp.pl>
2024-02-15 22:12:53 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,28 +3,48 @@ import { fetchBreeds, fetchCatByBreed } from './api-cat';
 const breedSelectHtml = document.querySelector('.breed-select');
 const errorHtml = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader-wrapper');
 
 errorHtml.classList.add('hidden');
+loader.classList.remove('hidden');
 
 try {
-  fetchBreeds().then(data => renderBreeds(data));
+  fetchBreeds()
+    .then(data => {
+      renderBreeds(data);
+      loader.classList.add('hidden');
+    })
+    .catch(error => {
+      console.error('Error fetching breeds:', error);
+      errorHtml.classList.remove('hidden');
+      loader.classList.add('hidden');
+    });
 } catch (error) {
+  console.error('Error fetching breeds:', error);
   errorHtml.classList.remove('hidden');
-  errorHtml.classList.add('show');
+  loader.classList.add('hidden');
 }
 
 function renderBreeds(breeds) {
   const markup = breeds
-    .map(({ name, id }) => {
-      return `<option value="${id}">${name}</option>`;
-    })
+    .map(({ name, id }) => `<option value="${id}">${name}</option>`)
     .join('');
   breedSelectHtml.innerHTML = markup;
 }
 
 breedSelectHtml.addEventListener('change', event => {
   const breedId = event.target.value;
-  fetchCatByBreed(breedId).then(catData => renderCat(catData));
+  loader.classList.remove('hidden');
+  fetchCatByBreed(breedId)
+    .then(catData => {
+      renderCat(catData);
+      loader.classList.add('hidden');
+    })
+    .catch(error => {
+      console.error('Error fetching cat:', error);
+      errorHtml.classList.remove('hidden');
+      loader.classList.add('hidden');
+    });
 });
 
 function renderCat(catData) {
